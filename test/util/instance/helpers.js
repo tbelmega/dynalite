2296fad4c221fc91e528818acba62fa55acd804d
Thiemo <thiemo.belmega@gmail.com>
2026-04-13 23:25:19 -0400

    Extract instance helper request

--- test/util/instance/helpers.js
+++ test/util/instance/helpers.js
@@ -1,8 +1,6 @@
 var http = require('http')
-var aws4 = require('aws4')
 var async = require('async')
-var once = require('once')
-var dynalite = require('../../..')
+var requestHelpers = require('./request')
 var safeCleanup = require('./safe-cleanup')
 
 var useRemoteDynamo = process.env.REMOTE
@@ -57,96 +55,7 @@ function createTestHelper (options) {
     { host: 'dynamodb.' + helper.awsRegion + '.amazonaws.com', method: 'POST' } :
     { host: '127.0.0.1', port: helper.port, method: 'POST' }
 
-  helper.startServer = function () {
-    return new Promise(function (resolve, reject) {
-      if (helper.useRemoteDynamo) {
-        // For remote DynamoDB, just set up tables and account ID
-        helper.createTestTables(function (err) {
-          if (err) return reject(err)
-          helper.getAccountId(resolve)
-        })
-        return
-      }
-
-      helper.server = dynalite({ path: process.env.DYNALITE_PATH })
-      helper.server.listen(helper.port, function (err) {
-        if (err) return reject(err)
-        helper.createTestTables(function (err) {
-          if (err) return reject(err)
-          helper.getAccountId(resolve)
-        })
-      })
-    })
-  }
-
-  helper.stopServer = function () {
-    return new Promise(function (resolve, reject) {
-      helper.deleteTestTables(function (err) {
-        if (err) return reject(err)
-        if (helper.server) {
-          helper.server.close(resolve)
-        }
-        else {
-          resolve()
-        }
-      })
-    })
-  }
-
-  // Helper functions already defined above
-
-  helper.request = function (opts, cb) {
-    if (typeof opts === 'function') { cb = opts; opts = {} }
-    opts.retries = opts.retries || 0
-    cb = once(cb)
-    for (var key in helper.requestOpts) {
-      if (opts[key] === undefined)
-        opts[key] = helper.requestOpts[key]
-    }
-    if (!opts.noSign) {
-      aws4.sign(opts)
-      opts.noSign = true // don't sign twice if calling recursively
-    }
-
-    var MAX_RETRIES = 20
-    http.request(opts, function (res) {
-      res.setEncoding('utf8')
-      res.on('error', cb)
-      res.rawBody = ''
-      res.on('data', function (chunk) { res.rawBody += chunk })
-      res.on('end', function () {
-        try {
-          res.body = JSON.parse(res.rawBody)
-        }
-        catch {
-          res.body = res.rawBody
-        }
-        if (helper.useRemoteDynamo && opts.retries <= MAX_RETRIES &&
-            (res.body.__type == 'com.amazon.coral.availability#ThrottlingException' ||
-            res.body.__type == 'com.amazonaws.dynamodb.v20120810#LimitExceededException')) {
-          opts.retries++
-          return setTimeout(helper.request, Math.floor(Math.random() * 1000), opts, cb)
-        }
-        cb(null, res)
-      })
-    }).on('error', function (err) {
-      if (err && ~[ 'ECONNRESET', 'EMFILE', 'ENOTFOUND' ].indexOf(err.code) && opts.retries <= MAX_RETRIES) {
-        opts.retries++
-        return setTimeout(helper.request, Math.floor(Math.random() * 100), opts, cb)
-      }
-      cb(err)
-    }).end(opts.body)
-  }
-
-  helper.opts = function (target, data) {
-    return {
-      headers: {
-        'Content-Type': 'application/x-amz-json-1.0',
-        'X-Amz-Target': helper.version + '.' + target,
-      },
-      body: JSON.stringify(data),
-    }
-  }
+  requestHelpers.attachInstanceRequest(helper)
 
   helper.getAccountId = function (done) {
     helper.request(helper.opts('DescribeTable', { TableName: helper.testHashTable }), function (err, res) {
---
+++ test/util/instance/request.js
@@ -0,0 +1,98 @@
+var http = require('http')
+var aws4 = require('aws4')
+var once = require('once')
+var dynalite = require('../../..')
+
+function attachInstanceRequest (helper) {
+  helper.startServer = function () {
+    return new Promise(function (resolve, reject) {
+      if (helper.useRemoteDynamo) {
+        helper.createTestTables(function (err) {
+          if (err) return reject(err)
+          helper.getAccountId(resolve)
+        })
+        return
+      }
+
+      helper.server = dynalite({ path: process.env.DYNALITE_PATH })
+      helper.server.listen(helper.port, function (err) {
+        if (err) return reject(err)
+        helper.createTestTables(function (err) {
+          if (err) return reject(err)
+          helper.getAccountId(resolve)
+        })
+      })
+    })
+  }
+
+  helper.stopServer = function () {
+    return new Promise(function (resolve, reject) {
+      helper.deleteTestTables(function (err) {
+        if (err) return reject(err)
+        if (helper.server) {
+          helper.server.close(resolve)
+        }
+        else {
+          resolve()
+        }
+      })
+    })
+  }
+
+  helper.request = function (opts, cb) {
+    if (typeof opts === 'function') { cb = opts; opts = {} }
+    opts.retries = opts.retries || 0
+    cb = once(cb)
+    for (var key in helper.requestOpts) {
+      if (opts[key] === undefined)
+        opts[key] = helper.requestOpts[key]
+    }
+    if (!opts.noSign) {
+      aws4.sign(opts)
+      opts.noSign = true
+    }
+
+    var MAX_RETRIES = 20
+    http.request(opts, function (res) {
+      res.setEncoding('utf8')
+      res.on('error', cb)
+      res.rawBody = ''
+      res.on('data', function (chunk) { res.rawBody += chunk })
+      res.on('end', function () {
+        try {
+          res.body = JSON.parse(res.rawBody)
+        }
+        catch {
+          res.body = res.rawBody
+        }
+        if (helper.useRemoteDynamo && opts.retries <= MAX_RETRIES &&
+            (res.body.__type == 'com.amazon.coral.availability#ThrottlingException' ||
+            res.body.__type == 'com.amazonaws.dynamodb.v20120810#LimitExceededException')) {
+          opts.retries++
+          return setTimeout(helper.request, Math.floor(Math.random() * 1000), opts, cb)
+        }
+        cb(null, res)
+      })
+    }).on('error', function (err) {
+      if (err && ~[ 'ECONNRESET', 'EMFILE', 'ENOTFOUND' ].indexOf(err.code) && opts.retries <= MAX_RETRIES) {
+        opts.retries++
+        return setTimeout(helper.request, Math.floor(Math.random() * 100), opts, cb)
+      }
+      cb(err)
+    }).end(opts.body)
+  }
+
+  helper.opts = function (target, data) {
+    return {
+      headers: {
+        'Content-Type': 'application/x-amz-json-1.0',
+        'X-Amz-Target': helper.version + '.' + target,
+      },
+      body: JSON.stringify(data),
+    }
+  }
+}
+
+module.exports = {
+  attachInstanceRequest: attachInstanceRequest,
+}
